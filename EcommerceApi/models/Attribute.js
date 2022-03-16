e867f9ddf72dd065ede48fb8236f640501798943
kudo9c <kudo9c88@gmail.com>
2022-03-16 22:04:39 +0700

    node_modules be gone!

--- EcommerceApi/models/Attribute.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const AttributeSchema = new mongoose.Schema({
-    label: {type: String, required:true}
-    }, 
-    {timestamps:true}
-)
-
-module.exports = mongoose.model("Attribute", AttributeSchema)
